745d8dff50e158c82b3af56b1e76b7619164172f
JoralmoPro <JoralmoPro@gmail.com>
2020-05-09 23:35:23 -0500

    obteniendo el token solo cuando se va a usar

--- src/apollo/apollo.js
+++ src/apollo/apollo.js
@@ -1,14 +1,13 @@
 import { execute, makePromise } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 
-const uri = 'https://siembre.herokuapp.com/v1/graphql';
-const token = localStorage.getItem('siembreAppToken');
-const link = new HttpLink({
-    uri,
-    headers: { authorization: token ? 'Bearer ' + token : '' },
-});
-
-function makeQuery (operation) {
+function makeQuery(operation) {
+    const uri = 'https://siembre.herokuapp.com/v1/graphql';
+    const token = localStorage.getItem('siembreAppToken');
+    const link = new HttpLink({
+        uri,
+        headers: { authorization: token ? 'Bearer ' + token : '' },
+    });
     return makePromise(execute(link, operation));
 }
 
